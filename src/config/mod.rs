879e1646d88a878800c0e854857c46300597d75e
Brian L. Troutwine <brian@troutwine.us>
2022-02-18 10:44:03 -0800

    [RETYPIST] applied 1 modifications

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -102,7 +102,7 @@ impl ConfigPath {
 pub struct Config {
     #[cfg(feature = "api")]
     pub(super) api: api::Options,
-    pub version: Option<String>,
+    pub(super) version: Option<String>,
     #[cfg(feature = "datadog-pipelines")]
     pub(super) datadog: Option<datadog::Options>,
     pub(super) global: GlobalOptions,
